3990d28a3ef4516c8fc28c9e9bcfbeb4cd0ff2c4
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2023-11-22 00:07:35 +0100

    task5: added cocktail sort

--- sort.h
+++ sort.h
@@ -24,5 +24,6 @@ void selection_sort(int *array, size_t size);
 listint_t *swap_node(listint_t *node, listint_t **list);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
