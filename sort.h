eda23a6e3c929c0ec8844bad52136bfdeb2341f7
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2023-11-22 00:24:02 +0100

    task6: added counting sort

--- sort.h
+++ sort.h
@@ -25,5 +25,6 @@ listint_t *swap_node(listint_t *node, listint_t **list);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 #endif
